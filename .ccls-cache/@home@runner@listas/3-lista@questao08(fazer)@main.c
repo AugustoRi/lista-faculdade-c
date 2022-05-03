b55e38f78cd5d2e338eb7b5c2c023a61392816f5
Augusto Ribeiro <68623782+AugustoRi@users.noreply.github.com>
2022-05-03 13:22:07 +0000

    atualizando questao 8

--- .ccls-cache/@home@runner@listas/3-lista@questao08(fazer)@main.c
+++ .ccls-cache/@home@runner@listas/3-lista@questao08(fazer)@main.c
@@ -5,7 +5,63 @@ de ordena ̧c ̃ao por sele ̧c ̃ao e outra pelo m ́etodo bolha (bubble sort).
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TAM 10
+
+void imprimir_vetor(int v[], int tamanho);
 
 int main() {
+  int v[TAM], selecao[TAM], bolha[TAM], min, max, nao_ordenado;
+  srand(time(NULL));
+
+  printf("Informe o limite minimo do intervalo:\n");
+  scanf("%d", &min);
+
+  printf("Informe o limite maximo do intervalo:\n");
+  scanf("%d", &max);
+
+  for (int i = 0; i < TAM; i++) {
+    v[i] = min + ( rand() % (max+1) );
+  }
   
+  for (int i = 0; i < TAM; i++) {
+    selecao[i] = v[i];
+    bolha[i] = v[i];
+  }
+
+    for (int i = 0; i < TAM - 1; i++) {
+      for (int k = i + 1; k < TAM; k++) {
+        if (selecao[i] > selecao[k]) {
+          selecao[i] = selecao[i] ^ selecao[k];
+          selecao[k] = selecao[i] ^ selecao[k];
+          selecao[i] = selecao[k] ^ selecao[k];
+          //c = selecao[i];
+          //v[i] = v[k];
+          //v[k] = c;
+        }
+      }
+  }
+
+    do {
+      nao_ordenado = 0;
+      for (int k = 0; k < TAM - 1; k++) {
+        if (bolha[k] > bolha[k+1]) {
+          bolha[k] = bolha[k] ^ bolha[k+1];
+          bolha[k+1] = bolha[k] ^ bolha[k+1];
+          bolha[k] = bolha[k] ^ bolha[k+1];
+          nao_ordenado = 1;
+        }
+      }
+    } while(nao_ordenado);
+
+  return 0;
+}
+
+void imprimir_vetor(int v[], int tamanho) {
+  for (int a = 0; a < tamanho; a++) {
+    printf("%d ", v[a]);
+  }
+  printf("\n");
 }
---
+++ .ccls-cache/@home@runner@listas/3-lista@questao08(fazer)@teste.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TAM 10
+
+int main() {
+  int v[TAM], selecao[TAM], bolha[TAM];
+  srand(time(NULL));
+
+  for (int i = 0; i < TAM; i++) {
+    v[i] = min + ( rand() % (max+1) );
+  }
+  
+  selecao[TAM ]
+}
